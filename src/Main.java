d324012a35862a1161333f4ab3710a894be274f2
Exagor <alexandre.achten@gmail.com>
2023-10-20 14:28:09 +0200

    variable table

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 import java.io.FileReader;
 import java.io.File;
+import java.util.HashMap;
 
 class Main {
+    private static HashMap<String, Integer> symbolTable = new HashMap<>();
+
+    public static void addSymbolToTable(Symbol symbol){
+        if (symbol.getType() == LexicalUnit.VARNAME && !symbolTable.containsKey(symbol.getValue())) {
+            String varName = (String) symbol.getValue();
+            symbolTable.put(varName, symbol.getLine());
+        }
+    }
+
+    public static void printSymbolTable() {
+        System.out.println("Variables :");
+        for (String varName : symbolTable.keySet()) {
+            System.out.println(varName + "   " + symbolTable.get(varName));
+        }
+    }
 
     public static void main(String[] argv){
         if (argv.length != 1){
@@ -18,18 +34,15 @@ class Main {
             Symbol symbol = lexicalAnalyzer.nextSymbol();
             while(symbol.getType() != LexicalUnit.EOS){
                 System.out.println(symbol.toString());
+                addSymbolToTable(symbol);
                 symbol = lexicalAnalyzer.nextSymbol();
             }
+            printSymbolTable(); //print the symbol table
         }
         catch(Exception e){
             System.out.println("Error while opening file: "+argv[0]);
             System.exit(1);
         }
-        
-
-        
-
-        
     
     }
 }
